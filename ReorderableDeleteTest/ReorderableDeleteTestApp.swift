1c97d4a9576dd6fd84bf52fa9ba57ee683178208
Jimmy Hooker <jimmyjjwalker@gmail.com>
2024-09-29 23:16:44 -0700

    Initial Commit

---
+++ ReorderableDeleteTest/ReorderableDeleteTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReorderableDeleteTestApp.swift
+//  ReorderableDeleteTest
+//
+//  Created by Jimmy Hooker on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ReorderableDeleteTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReorderableDeleteTestTests/ReorderableDeleteTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReorderableDeleteTestTests.swift
+//  ReorderableDeleteTestTests
+//
+//  Created by Jimmy Hooker on 9/29/24.
+//
+
+import Testing
+@testable import ReorderableDeleteTest
+
+struct ReorderableDeleteTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
